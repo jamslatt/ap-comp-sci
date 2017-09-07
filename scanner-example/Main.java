5047903c82bf1add94010c52f6d8d9f1d35229d6
James Slattery <james@slattery.tech>
2017-09-07 11:09:06 -0700

    Scannner

---
+++ scanner-example/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Welcome to ________!");
+        System.out.println("What would you like to type");
+        String input = scanner.next();
+        System.out.println("Ok we will use " + input);
+        System.out.println("Hello world! Welcome to " + input);
+        
+        
+        
+        
+    }
+}
