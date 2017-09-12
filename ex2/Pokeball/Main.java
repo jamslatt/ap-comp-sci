85fdf3cf788b85b9b4b91eeecf4ebe4efcc5856f
James Slattery <james@slattery.tech>
2017-09-12 09:29:59 -0700

    Pokeball

---
+++ ex2/Pokeball/Main.java
@@ -0,0 +1,23 @@
+
+/**
+ By: James Slattery
+ Pokeball
+ Period 1
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  @@@@@@@@@@  ");
+        System.out.println(" @@@@@@@@@@@@ ");
+        System.out.println("@@@@@@@@@@@@@@");
+        System.out.println("|||||[\"\"\"\"]|||||");
+        System.out.println("|||||[....]|||||");
+        System.out.println("OOOOOOOOOOOOOOOO");
+        System.out.println(" OOOOOOOOOOOOOO ");
+        System.out.println("  OOOOOOOOOOOO  ");
+        System.out.println("   OOOOOOOOOO   ");
+        System.out.println();
+        // This Pkeball graphic can be used in the opening sequence of my Pokemon game, or any time the player needs to catch a whild Pokemon.
+        
+        
+    }
+}
