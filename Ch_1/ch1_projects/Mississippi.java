0f145efabd70f7945041442077f72f45a5f1bd20
James Slattery <james@slattery.tech>
2017-09-14 10:08:47 -0700

    Mississippii

---
+++ Ch_1/ch1_projects/Mississippi.java
@@ -0,0 +1,17 @@
+
+/////////////////////
+// By: James Slattery
+// Ch 1 Project 1
+/////////////////////
+public class Mississippi {
+    public static void main(String[] args) {
+        System.out.println("M     M   IIIII    SSSSS     SSSSS    IIIII    SSSSS     SSSSS  IIIII   PPPPPP   PPPPPP  IIIII");
+        System.out.println("MM   MM     I     S     S   S     S     I     S     S   S     S   I    P     P  P     P    I");
+        System.out.println("M M M M     I     S         S           I     S         S         I    P     P  P     P    I");
+        System.out.println("M  M  M     I      SSSSS     SSSSS      I      SSSSS     SSSSS    I    PPPPPP   PPPPPP     I");
+        System.out.println("M     M     I           S         S     I           S         S   I    P        P          I");
+        System.out.println("M     M     I     S     S   S     S     I     S     S   S     S   I    P        P          I");
+        System.out.println("M     M   IIIII    SSSSS     SSSSS    IIIII    SSSSS     SSSSS  IIIII  P        P        IIIII");   
+    }
+
+}
