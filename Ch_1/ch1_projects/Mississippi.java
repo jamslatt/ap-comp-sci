64f2072d38728a3221e79ba3be26f818368962ec
James Slattery <james@slattery.tech>
2017-09-14 10:17:17 -0700

    Lyrics

--- Ch_1/ch1_projects/Mississippi.java
+++ Ch_1/ch1_projects/Mississippi.java
@@ -14,4 +14,43 @@ public class Mississippi {
         System.out.println("M     M   IIIII    SSSSS     SSSSS    IIIII    SSSSS     SSSSS  IIIII  P        P        IIIII");   
     }
 
+    public static void lyrics() {
+        System.out.println("There was an old lady who swallowed a fly.");
+        System.out.println("I don't know why she swallowed that fly,");
+        System.out.println("Perhaps she’ll die.");
+        System.out.println();
+        System.out.println("Chapter 1 Introduction to Java Programming");
+        System.out.println("There was an old lady who swallowed a spider,");
+        System.out.println("That wriggled and iggled and jiggled inside her.");
+        System.out.println("She swallowed the spider to catch the fly,");
+        System.out.println("I don't know why she swallowed that fly,");
+        System.out.println("Perhaps she'll die.");
+        System.out.println();
+        System.out.println("There was an old lady who swallowed a bird,");
+        System.out.println("How absurd to swallow a bird.");
+        System.out.println("She swallowed the bird to catch the spider,");
+        System.out.println("She swallowed the spider to catch the fly,");
+        System.out.println("I don't know why she swallowed that fly,");
+        System.out.println("Perhaps she'll die.");
+        System.out.println();
+        System.out.println("There was an old lady who swallowed a cat,");
+        System.out.println("Imagine that to swallow a cat.");
+        System.out.println("She swallowed the cat to catch the bird,");
+        System.out.println("She swallowed the bird to catch the spider,");
+        System.out.println("She swallowed the spider to catch the fly,");
+        System.out.println("I don't know why she swallowed that fly,");
+        System.out.println("Perhaps she'll die.");
+        System.out.println();
+        System.out.println("There was an old lady who swallowed a dog,");
+        System.out.println("What a hog to swallow a dog.");
+        System.out.println("She swallowed the dog to catch the cat,");
+        System.out.println("She swallowed the cat to catch the bird,");
+        System.out.println("She swallowed the bird to catch the spider,");
+        System.out.println("She swallowed the spider to catch the fly,");
+        System.out.println("I don't know why she swallowed that fly,");
+        System.out.println("Perhaps she'll die.");
+        System.out.println();
+        System.out.println("There was an old lady who swallowed a horse,");
+        System.out.println("She died of course.");
+    }
 }
