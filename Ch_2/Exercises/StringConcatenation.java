5a88faac26af0ffd263f9241d27cb26b7cdacd4d
James Slattery <james@slattery.tech>
2017-09-26 10:21:53 -0700

    String concatenation / i++ --

---
+++ Ch_2/Exercises/StringConcatenation.java
@@ -0,0 +1,29 @@
+
+/////////////////////
+// By: James Slattery
+// In Class Work
+// Found Online at github.com/jamslatt/ap-comp-sci
+////////////////////  
+
+public class StringConcatenation {
+    public static void main(String[] args) {
+        System.out.println(1 + 2 + 3 + "chololate");
+        System.out.println("chololate" + 1 + 2 + 3);
+        System.out.println("chololate" + (1+2) + 3);
+        System.out.println("chololate" + (1+2)/3);
+        System.out.println();
+        System.out.println("Spone bob thinks " + "the best time to wear a sweater" + "is all the time!");
+        System.out.println("Here we combine " + 1 + " integer" + " with strings!");
+        System.out.println(42 + " is the answer to" + "everything!" + 1 + 1);
+        System.out.println();
+        
+        int x = 0;
+        
+        x += 1;
+        
+        System.out.println(x);
+    
+    
+    }
+}
+
