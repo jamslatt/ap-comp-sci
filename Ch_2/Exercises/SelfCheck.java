3014909954699599a631a9cda654875a98c08c41
James Slattery <james@slattery.tech>
2017-09-25 18:46:49 -0700

    Ch3 Stuff

---
+++ Ch_2/Exercises/SelfCheck.java
@@ -0,0 +1,33 @@
+
+/////////////////////
+// By: James Slattery
+// Ch 2 Self Check
+// Found Online at github.com/jamslatt/ap-comp-sci
+////////////////////  
+
+public class SelfCheck {
+    public static void main(String[] args) {
+        System.out.println("#2");
+        System.out.println(2 + 3 * 4 - 6);
+        System.out.println(14 / 7 * 2 + 30 / 5 + 1);
+        System.out.println((12 + 3) / 4 * 2);
+        System.out.println((238 % 10 + 3) % 7);
+        System.out.println((18 - 7) * (43 % 10));
+        System.out.println();
+        System.out.println("#3");
+        System.out.println(4.0 / 2 * 9 / 2);
+        System.out.println(2.5 * 2 + 8 / 5.0 + 10 / 3);
+        System.out.println(12 / 7 * 4.4 * 2 / 4);
+        System.out.println(4 * 3 / 8 + 2.5 * 2);
+        System.out.println((5 * 7.0 / 2 - 2.5) / 5 * 2);
+        System.out.println();
+        System.out.println("#4");
+        System.out.println(2 + 2 + 3 + 4);
+        System.out.println("2 + 2" + 3 + 4);
+        System.out.println(2 + " 2 + 3 " + 4);
+        System.out.println(3 + 4 + " 2 + 2");
+        System.out.println("2 + 2 " + (3 + 4));
+        System.out.println();
+    }
+}
+
