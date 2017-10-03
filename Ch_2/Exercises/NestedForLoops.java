6b42b5208916305a8db7921d4672093b67783e5d
James Slattery <james@slattery.tech>
2017-10-03 09:11:24 -0700

    Finetune ch2 ex2/3 and start in class lesson class

---
+++ Ch_2/Exercises/NestedForLoops.java
@@ -0,0 +1,17 @@
+
+/////////////////////
+// By: James Slattery
+// Ch _ Ex _
+// Found Online at github.com/jamslatt/ap-comp-sci
+////////////////////  
+
+public class NestedForLoops {
+    public static void main(String[] args) {
+        for ( int i = 1; i<=3; i++) {
+            for ( int j=1 ; j<=3; j++) {
+                System.out.println("Hello");
+            }
+        }
+    }
+}
+
--- Ch_2/Exercises/SlatteryJames_Chap2Ex2_3.java
+++ Ch_2/Exercises/SlatteryJames_Chap2Ex2_3.java
@@ -8,10 +8,14 @@ public class SlatteryJames_Chap2Ex2_3 {
         two();
         three();
     }
-
+    
+    // Static Method for Ex 2
     public static void two() {
+        // Loop that runs 10 times
         for (int i=1; i<= 10; i++) {
+            // Sets variable f to i to the power of two
             int f = (int)Math.pow(i, 2);
+            // Prints f value to console
             System.out.println(f);
         }
     }
