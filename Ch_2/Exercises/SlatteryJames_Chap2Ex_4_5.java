c76616fdab5b995c12553b39c1736732bd5919bf
James Slattery <james@slattery.tech>
2017-10-05 08:54:19 -0700

    Right way

--- Ch_2/Exercises/SlatteryJames_Chap2Ex_4_5.java
+++ Ch_2/Exercises/SlatteryJames_Chap2Ex_4_5.java
@@ -7,9 +7,13 @@
 
 public class SlatteryJames_Chap2Ex_4_5 {
     public static void main(String[] args) {
-        
+        four();
+        five();
     }
     
+    ////////////////
+    // Ex 4
+    ///////////////
     public static void four() {
         for (int i=0; i<4; i++) {
          for(int j=0; j<5; j++) {
@@ -19,17 +23,15 @@ public class SlatteryJames_Chap2Ex_4_5 {
         }
     }
     
-   
+    /////////////////
+    // Ex 5
+    ////////////////
     public static void five() {
         for (int o=1; o<=5; o++) {
-         stars(o);
+         for(int j=1; j<=o; j++) {
+             System.out.print("*");
+         }
          System.out.println();
-        }
-    }
-    
-    public static void stars(int amt) {
-        for (int i=0; i<amt; i++) {
-            System.out.print("*");
         }
     }
 }
