4a86376e9454dbc8a7686cbe87359a73849ce87c
James Slattery <james@slattery.tech>
2017-10-16 09:00:59 -0700

    paramaters

---
+++ Ch_3/Exercises/Parameters.java
@@ -0,0 +1,17 @@
+
+/////////////////////////////////////////////////////////
+// By James Slattery
+// Ch _ Ex _
+// View online at https://github.com/jamslatt/ap-comp-sci
+//////////////////////////////////////////////////////////
+
+
+public class Parameters {
+    public static void main(String[] args) {
+        name("James");
+    }
+    
+    public static void name(String Print) {
+        System.out.print(Print);
+    }
+}
