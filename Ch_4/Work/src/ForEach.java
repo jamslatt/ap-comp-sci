7432ee3da74aa7c6652cf6036015c4f395737405
James Slattery <james@slattery.tech>
2017-11-20 21:43:55 -0800

    Still need to finish addGuide and others

---
+++ Ch_4/Work/src/ForEach.java
@@ -0,0 +1,23 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int [] fallTemps = {29,19,8,9,73,91};
+        int huga = 0;
+
+        for(int chonga: fallTemps) {
+            if (chonga >= 32)
+                huga++;
+        }
+
+        int[] array = {1,3,9,2,10,8};
+
+        max(array);
+
+        System.out.println(huga);
+    }
+
+    public static void max(int[] array) {
+        for(int chona : array) {
+            
+        }
+    }
+}
