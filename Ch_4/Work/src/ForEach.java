eaaf595c2322fb735b13c0c1fcfcbc4b1ee5cb4e
James Slattery <james@slattery.tech>
2017-11-21 08:54:59 -0800

    Worksheet work

--- Ch_4/Work/src/ForEach.java
+++ Ch_4/Work/src/ForEach.java
@@ -1,23 +1,63 @@
+import java.util.Arrays;
+
 public class ForEach {
     public static void main(String[] args) {
-        int [] fallTemps = {29,19,8,9,73,91};
-        int huga = 0;
-
-        for(int chonga: fallTemps) {
-            if (chonga >= 32)
-                huga++;
-        }
-
         int[] array = {1,3,9,2,10,8};
+        setAllToFortyTwo(array);
 
-        max(array);
+    }
 
-        System.out.println(huga);
+    public static void setAllToFortyTwo(int[] array) {
+        for(int chonga = 0; chonga<array.length; chonga++) {
+            array[chonga] = 42;
+        }
     }
 
     public static void max(int[] array) {
         for(int chona : array) {
-            
+
         }
     }
+
+    public static void average(int[] array) {
+        int sum = 0;
+        int length = array.length;
+        for(int chona : array) {
+            sum +=chona;
+        }
+
+        double average = sum / length;
+
+        System.out.println("The average is: " + average);
+    }
+
+    public static void doubble(int[] array) {
+        for(int chona =0; chona<array.length; chona++) {
+            array[chona] = array[chona] * 2;
+        }
+    }
+
+    public static void zerotothree(int[] array) {
+        for(int chona =0; chona<3; chona++) {
+            System.out.print("Index " + chona + " " +array[chona]);
+        }
+    }
+
+
+
+    //3
+
+
+    public static void range(int[] array) {
+        Arrays.sort(array);
+
+        int num = (array[array.length] - array[0]) + 1;
+
+        System.out.println("Range is " + num);
+    }
+
+
+    //4
+
+
 }
