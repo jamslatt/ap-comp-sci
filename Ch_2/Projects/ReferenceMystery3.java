952efd558300fa6d2aaba65f5d98ffdee2071977
James Slattery <james@slattery.tech>
2018-01-04 08:46:57 -0800

    Pt. Class

---
+++ Ch_2/Projects/ReferenceMystery3.java
@@ -0,0 +1,25 @@
+
+/////////////////////////////////////////////////////////
+// By James Slattery
+// Ch _ Ex _
+// View online at https://github.com/jamslatt/ap-comp-sci
+//////////////////////////////////////////////////////////
+
+
+public class ReferenceMystery3 {
+public static void main(String[] args) {
+int a = 7;
+int b = 9;
+Point p1 = new Point(2, 2);
+Point p2 = new Point(2, 2);
+addToXTwice(a, p1);
+System.out.println(a + " " + b + " " + p1.x + " " + p2.x);
+addToXTwice(b, p2);
+System.out.println(a + " " + b + " " + p1.x + " " + p2.x);
+}
+public static void addToXTwice(int a, Point p1) {
+a = a + a;
+p1.x = a;
+System.out.println(a + " " + p1.x);
+}
+}
