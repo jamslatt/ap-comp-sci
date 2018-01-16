ba51db4a5b1e4baf8365ac53edff615085998b5f
James Slattery <james@slattery.tech>
2018-01-16 09:01:47 -0800

    Inital MagPie

---
+++ MagPie/src/MagPieRunner2.java
@@ -0,0 +1,33 @@
+// By James Slattery     //
+// GitHub: @jamslatt     //
+///////////////////////////
+
+import java.util.Scanner;
+
+/**
+ * A simple class to run the Magpie class.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class MagpieRunner2
+{
+
+    /**
+     * Create a Magpie, give it user input, and print its replies.
+     */
+    public static void main(String[] args)
+    {
+        MagPie2 maggie = new MagPie2();
+
+        System.out.println (maggie.getGreeting());
+        Scanner in = new Scanner (System.in);
+        String statement = in.nextLine();
+
+        while (!statement.equals("Bye"))
+        {
+            System.out.println (maggie.getResponse(statement));
+            statement = in.nextLine();
+        }
+    }
+
+}
