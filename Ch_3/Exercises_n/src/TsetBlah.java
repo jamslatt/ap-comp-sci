2c36acffd5ddcadda3a96fa2b801f4e8a5dbce3d
James Slattery <james@slattery.tech>
2018-01-22 20:53:46 -0800

    m

---
+++ Ch_3/Exercises_n/src/TsetBlah.java
@@ -0,0 +1,20 @@
+/**
+ * Created by james on 1/22/2018.
+ */
+import java.util.ArrayList;
+public class TsetBlah {
+    public static void main(String[] args) {
+                ArrayList<String> schoolProgram = new
+                        ArrayList<String>(6);
+                schoolProgram.add("AP CS");
+                schoolProgram.add("Spanish");
+                schoolProgram.add("Calculus");
+                schoolProgram.add("English");
+                schoolProgram.add("History");
+                schoolProgram.add("Physics");
+// YOUR CODE CODE GOES HERE
+            schoolProgram.set(2,"Geometry");
+            
+            }
+
+    }
